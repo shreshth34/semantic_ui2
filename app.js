90caeeeba073429170f2e1919ca14bb3ca57f5ea
Shreshth  Juneja <INTERNAL\shreshth.j@shreshth.internal.talview.com>
2020-02-18 19:02:03 +0530

    open index.html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const color = ["linear-gradient(to right, #b2fefa, #0ed2f7)","linear-gradient(to right, #a770ef, #cf8bf3, #fdb99b)","linear-gradient(to right, #f12711, #f5af19)" ,"linear-gradient(to right, #ee9ca7, #ffdde1)","linear-gradient(to right, #0f0c29, #302b63, #24243e)","linear-gradient(to right, #283048, #859398)", "linear-gradient(to right, #03001e, #7303c0, #ec38bc, #fdeff9)", "linear-gradient(to right, #ba8b02, #181818)", "linear-gradient(to right, #f0c27b, #4b1248)", "linear-gradient(to right, #000428, #004e92)"];
 
+let Carr = [];
 function myFunction() {
     const randomColor = color[Math.floor(Math.random() * color.length)];
 
@@ -20,6 +21,8 @@ function newFunction(){
 }
 
 
+
+
 function oneFunction(){
 
   var x = document.getElementById("past_exp_box");
@@ -37,7 +40,6 @@ document.getElementById('cert_right').onclick = () => {
   
 
 }
-
 
 
 
